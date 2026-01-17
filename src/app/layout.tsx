cbc92d48ff2ec2b5e742aeabae9f3f5073b4921d
Dennis Kolawole <lucasdeekay98@gmail.com>
2026-01-17 17:33:30 +0100

    fix: updated import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "globals.css";
+import "../globals.css";
 import { LoadingProvider } from "../context/LoadingContext";
 
 const inter = Inter({ subsets: ["latin"] });
